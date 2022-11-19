ddf83f0eeef6582f6b28b9271e3e0fb24f378842
NasVa <aska.vakulchik@mail.ru>
2022-11-19 23:43:22 +0300

    using session to count visits of site

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -8,11 +8,26 @@ import java.io.PrintWriter;
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+
+        Integer count = (Integer)session.getAttribute("count");
+        if (count == null){
+            session.setAttribute("count", 1);
+            count = 1;
+        }
+        else{
+            session.setAttribute("count", count + 1);
+        }
+
         PrintWriter writer =response.getWriter();
-        String username = request.getParameter("name");
+        //String username = request.getParameter("name");
         writer.println("<html>");
-        writer.println("<h3>salamm, dear " + username + "</h3>");
+        writer.println("<h3>salamm, your count: " + count + " times.</h3>");
         writer.println("</html>");
+
+       // response.sendRedirect("/greeting.jsp?myname=Nastya");
+        //RequestDispatcher dispatcher = request.getRequestDispatcher("/greeting.jsp?myname=Nastya");
+        //dispatcher.forward(request,response);
     }
 
     @Override
